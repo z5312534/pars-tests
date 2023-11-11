f0abf5a6d8ce94cb6edbacf43c2f26e8c0c49c41
z5312534 <z5312534@unsw.edu.au>
2023-11-11 17:34:29 +1100

    add tests for nproc / nproc + 1 jobs

---
+++ tests/1.2/07.sh
@@ -0,0 +1,21 @@
+# test `nproc` + 1 jobs
+
+nproc=$(nproc)
+
+in="$(yes "sleep 5; echo 1" | head -"$nproc")
+echo 2
+echo 2"
+
+expected="2
+2
+$(yes "1" | head -"$nproc")"
+
+out=$($pars -J $((nproc+1)) <<<"$in")
+
+if diff -q <(echo "$expected") <(echo "$out") >/dev/null 2>&1; then
+	echo -e "$0 \033[0;32m passed\033[0;0m"
+else
+	echo -e "$0 \033[0;31m failed\033[0;0m"
+	echo "$out" >"$0.out"
+	echo "$expected" >"$0.expected"
+fi
