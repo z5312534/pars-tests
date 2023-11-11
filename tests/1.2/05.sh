9e8a81d85e35cf2705395e08420c3c02c37f19e6
z5312534 <z5312534@unsw.edu.au>
2023-11-11 15:17:07 +1100

    add test for a large job number

---
+++ tests/1.2/05.sh
@@ -0,0 +1,85 @@
+# test a large number of jobs
+
+in=$(cat <<EOF
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+sleep 5; echo 1
+echo 2
+echo 2
+EOF
+)
+
+expected=$(cat <<EOF
+2
+2
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+EOF
+)
+
+out=$($pars -J 32 <<<"$in")
+
+if diff -q <(echo "$expected") <(echo "$out") >/dev/null 2>&1; then
+	echo -e "$0 \033[0;32m passed\033[0;0m"
+else
+	echo -e "$0 \033[0;31m failed\033[0;0m"
+	echo "$out" >"$0.out"
+	echo "$expected" >"$0.expected"
+fi
